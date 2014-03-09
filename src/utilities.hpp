75e0f8f79f49eaf50d29b5b1b92fe372ea39b563
Chafic Najjar <chafic.najjar@gmail.com>
2014-03-10 00:57:06 +0200

    removed useless function definitions

--- src/utilities.hpp
+++ src/utilities.hpp
@@ -3,15 +3,11 @@
  *  Useful functions
  */
 
-void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, SDL_Rect dst, SDL_Rect *clip = nullptr);
-void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, SDL_Rect *clip = nullptr);
-SDL_Texture* renderText(const std::string &message, const std::string &fontFile, SDL_Color color, int fontSize, SDL_Renderer *renderer);
-
-void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, SDL_Rect dst, SDL_Rect *clip) {
+void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, SDL_Rect dst, SDL_Rect *clip = nullptr) {
     SDL_RenderCopy(ren, tex, clip, &dst);
 }
 
-void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, SDL_Rect *clip) {
+void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, SDL_Rect *clip = nullptr) {
     SDL_Rect dst;
     dst.x = x;
     dst.y = y;
